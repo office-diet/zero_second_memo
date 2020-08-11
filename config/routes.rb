9628b5e39d46be0244e63b973dd9479d42d3cee9
office-diet <kw278u@bma.biglobe.ne.jp>
2020-08-11 18:00:14 +0900

    heroku　仮デプロイ準備

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'memos#new'
-  resources :memos, only: [:inde, :new, :create, :show]
+  resources :memos, only: [:new, :create, :show]
 end
