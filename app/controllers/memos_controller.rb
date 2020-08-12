39c1ecbe6a27472bc0e94226f4f87a63de6fee7b
office-diet <kw278u@bma.biglobe.ne.jp>
2020-08-12 12:44:19 +0900

    日別・タグ別ルーティング実装

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -2,9 +2,11 @@ class MemosController < ApplicationController
   before_action :set_memos, only: [:index, :new, :show, :tag]
   def index
   end
+  
   def new
-    @memo_tag = MemoTag.new
+    @new_memo = MemoTag.new
     @date = Date.today.to_date
+    @list_memos = @memos.where(created_at: @date.in_time_zone.all_day)
   end
 
   def create
@@ -20,21 +22,28 @@ class MemosController < ApplicationController
 
   def show
     if params[:date].present?
-      @date = params[:id].to_date
+      @date = params[:date].to_date
+      @list_memos = @memos.where(created_at: @date.in_time_zone.all_day)
+      @memo = @memos.where(created_at: @date.in_time_zone.all_day).first
     else
-      @memo_id = params[:id]
-      @date = Memo.find(params[:id]).created_at.to_date
+      @memo = @memos.find(params[:id])
+      @date = @memo.created_at.to_date
+      @list_memos = @memos.where(created_at: @date.in_time_zone.all_day)
     end
   end
 
   def tag
-    memo_ids = MemosTag.where(tag_id: params[:id]).select(:memo_id)
-    @memos_tags_tag = Memo.includes(:tags).where(id: memo_ids).order(id: "desc")
+      @tag_id = params[:tag]
+      memo_ids = MemosTag.where(tag_id: @tag_id).select(:memo_id)
+      @list_memos = Memo.includes(:tags).where(id: memo_ids).order(id: "desc")
+      @memo = @list_memos.find(params[:id])
+      @tag = Tag.find(params[:tag]).name
   end
 
   private
   def set_memos
-    @memos_tags = Memo.includes(:tags).where(user_id: current_user.id).order(id: :desc)
+    @memos = Memo.includes(:tags).where(user_id: current_user.id).order(id: :desc)
+    @side_memos = @memos.select(:created_at, :theme)
   end
   
   def memo_params
