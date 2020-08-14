c125d9aa8901e59b7ba9597e9e01fa3ac7b56daa
office-diet <kw278u@bma.biglobe.ne.jp>
2020-08-14 21:55:25 +0900

    バグ調査01

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,6 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
+  config.action_mailer.default_url_options = { :host => 'zero-second-memo.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
