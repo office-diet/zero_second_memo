4579032eb65a4500352033ae4bf87d0ac66a662d
office-diet <kw278u@bma.biglobe.ne.jp>
2020-08-15 10:24:23 +0900

    herokuメール機能テスト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,15 +110,30 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   port:                 587,
+  #   address:              'smtp.gmail.com',
+  #   domain:               'gmail.com',
+  #   user_name:            'officediet.twitter@gmail.com',
+  #   password:             ENV["GMAIL_PASSWORD"],
+  #   authentication:       'login',
+  #   enable_starttls_auto: true
+  # }
+
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { :host => 'zero-second-meno.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    port:                 587,
-    address:              'smtp.gmail.com',
-    domain:               'gmail.com',
-    user_name:            'officediet.twitter@gmail.com',
-    password:             ENV["GMAIL_PASSWORD"],
-    authentication:       'login',
-    enable_starttls_auto: true
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain => "heroku.com",
+    :address => "smtp.sendgrid.net",
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
+
 end
