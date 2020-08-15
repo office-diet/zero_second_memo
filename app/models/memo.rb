be78649550b9269430db8ee67655ece86bbafbf2
office-diet <kw278u@bma.biglobe.ne.jp>
2020-08-15 20:13:44 +0900

    Herokuデプロイ用機能制限

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,5 +1,5 @@
 class Memo < ApplicationRecord
   belongs_to :user
-  has_many :memos_tags
+  has_many :memos_tags, dependent: :destroy
   has_many :tags, through: :memos_tags
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
       get :tag
     end
   end
-  resources :users, only: [:edit, :update, :password]
+  # resources :users, only: [:edit, :update]
 end
